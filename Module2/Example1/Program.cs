4a5efe5d31bb17e50bbe0ded20d92db208eb4ef1
pjsm <pjsm.lesi@gmail.com>
2025-04-02 19:42:10 +0100

    Updated Program

--- Module2/Example1/Program.cs
+++ Module2/Example1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+float baseSalary = 50000;
+float hra = 0.3F;
+
+float netSalary = (baseSalary - (baseSalary * hra));
+
+Console.WriteLine("Base salary: " + baseSalary);
+Console.WriteLine("Net salary: " + netSalary);
+
+Console.Write("Enter the temperature: ");
+int temperature = int.Parse(Console.ReadLine());
+
+if (temperature <= 20)
+{
+    Console.WriteLine("It's cold outside.");
+}
+else
+{
+    Console.WriteLine("It's moderate outside.");
+}
+
+bool isLoogedIn = true;
+bool isEmailVerified = true;
+bool cardInfo = true;
+
+if (isLoogedIn && isEmailVerified && cardInfo)
+{
+    Console.WriteLine("Allowed to make a purchase.");
+}
+else
+{
+    Console.WriteLine("Stop purchase.");
+}
+
+Console.ReadKey();
