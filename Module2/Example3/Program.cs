5ede318a4105dddcc8321ce14a7f12c67b9cd947
pjsm <pjsm.lesi@gmail.com>
2025-04-02 20:07:06 +0100

    Created Example3

---
+++ Module2/Example3/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+string[] names = { "King Kochar", "Sarah Bowling", "John Smith", "Roger Lee", "James Lee" };
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+Console.ReadKey();
