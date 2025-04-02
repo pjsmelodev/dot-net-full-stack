174f4b887e8c0aa1d0bfb07363d4cc842ca98205
pjsm <pjsm.lesi@gmail.com>
2025-04-02 18:46:32 +0100

    Updated Program; Created Redundant project

--- coreConsoleApplication/coreConsoleApplication/Program.cs
+++ coreConsoleApplication/coreConsoleApplication/Program.cs
@@ -6,5 +6,17 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Let's learn C# Programming!");
 
 string name = "King Kochar";
+int age = 23;
+float discount = 1000.50F;
+double contactNumber = 1234567890;
+char gender = 'M';
+Boolean isValid = true;
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Discount: " + discount);
+Console.WriteLine("Contact Number: " + contactNumber);
+Console.WriteLine("Gender: " + gender);
+Console.WriteLine("Is Valid: " + isValid);
 
 Console.ReadKey();
