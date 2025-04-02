b16aa6a2c1087a13c06b9c62e1ada2460ea7fbfe
pjsm <pjsm.lesi@gmail.com>
2025-04-02 20:09:16 +0100

    Updated Example3

--- Module2/Example3/Program.cs
+++ Module2/Example3/Program.cs
@@ -7,4 +7,12 @@ for (int i = 0; i < names.Length; i++)
     Console.WriteLine(names[i]);
 }
 
+Console.WriteLine();
+
+// Cuidado para evitar o erro de "IndexOutOfRangeException"
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
 Console.ReadKey();
