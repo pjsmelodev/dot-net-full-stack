61024d218e164653cef9733a13828967faae00fe
pjsm <pjsm.lesi@gmail.com>
2025-04-03 23:07:42 +0100

    Created Example1

---
+++ Module3/Example1/Example1/IBankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    internal interface IManageBank
+    {
+        void OpenAccount();
+        void CloseAccount();
+    }
+
+    internal interface IBankAccount
+    {
+        // Todos os métodos de um Interface são públicos e abstratos por defeito
+        void Deposit();
+        void Withdraw();
+        void Balance();
+    }
+
+    public class SavingAcc : IBankAccount, IManageBank
+    {
+        public void Balance()
+        {
+            Console.WriteLine("Saving Account Balance");
+        }
+
+        public void CloseAccount()
+        {
+            Console.WriteLine("Closing Saving Account.");
+        }
+
+        public void Deposit()
+        {
+            Console.WriteLine("Saving Account Deposit");
+        }
+
+        public void OpenAccount()
+        {
+            Console.WriteLine("Opening Saving Account.");
+        }
+
+        public void Withdraw()
+        {
+            Console.WriteLine("Saving Account Withdraw");
+        }
+    }
+}
--- Module3/Example1/Example1/Program.cs
+++ Module3/Example1/Example1/Program.cs
@@ -5,7 +5,15 @@ SavingsAccount savingsAccount = new SavingsAccount();
 savingsAccount.Deposit();
 savingsAccount.Withdraw();
 savingsAccount.Balance();
-
 savingsAccount.GetMessage();
+
+Console.WriteLine("--------------------------------------------------");
+
+SavingAcc savingAcc = new SavingAcc();
+savingAcc.OpenAccount();
+savingAcc.Deposit();
+savingAcc.Withdraw();
+savingAcc.Balance();
+savingAcc.CloseAccount();
 
 Console.ReadKey();
