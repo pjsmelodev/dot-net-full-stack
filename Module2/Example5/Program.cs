aef52f2d96c8762cc5680a363eae4096779b0684
pjsm <pjsm.lesi@gmail.com>
2025-04-03 18:01:43 +0100

    Updated Program

--- Module2/Example5/Program.cs
+++ Module2/Example5/Program.cs
@@ -1,2 +1,15 @@
-﻿int[] marks = new int[5];
+﻿using System;
 
+int[] marks = new int[5];
+marks[0] = 25;
+marks[1] = 50;
+marks[2] = 55;
+marks[3] = 60;
+marks[4] = 40;
+
+foreach (int mark in marks)
+{
+    Console.WriteLine(mark);
+}
+
+Console.ReadKey();
