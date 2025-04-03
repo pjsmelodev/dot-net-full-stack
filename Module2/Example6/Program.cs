5f7f414ec6d30cdde65d7ff0e2c6c8897b33f530
pjsm <pjsm.lesi@gmail.com>
2025-04-03 18:26:02 +0100

    Created Example6

--- Module2/Example6/Program.cs
+++ Module2/Example6/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+string str1 = "Hello World";
+string str2 = "C# Programming";
+
+Console.WriteLine(str1);
+Console.WriteLine(str1.Length);
+Console.WriteLine(str1.Concat(str2));
+Console.WriteLine(str1.Equals(str2));
+
+// Immutable String
+
+string s1 = "C++ Programming";
+string s2 = "Java Programming";
+
+// Mutable String
+
+StringBuilder stringBuilder = new StringBuilder();
+
+stringBuilder.Append("C++ Programming");
+stringBuilder.Append("Java Programming");
+
+Console.WriteLine(stringBuilder.ToString());
+
+Console.ReadKey();
