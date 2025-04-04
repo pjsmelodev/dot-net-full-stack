6c7e44a9b6f1cfaf8ffb7316cd7f15d711ff562d
pjsm <pjsm.lesi@gmail.com>
2025-04-04 19:37:48 +0100

    Created Calculate; Updated Program

---
+++ Module3/Example1/Example1/Calculate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    static class Calculate
+    {
+        static int count = 0;
+
+        static Calculate()
+        {
+            count = 0;
+        }
+
+        public static int Increment()
+        {
+            count++;
+            return count;
+        }
+
+        public static int Decrement()
+        {
+            count--;
+            return count;
+        }
+    }
+}
--- Module3/Example1/Example1/Program.cs
+++ Module3/Example1/Example1/Program.cs
@@ -1,19 +1,27 @@
 ﻿using Example1;
 
-SavingsAccount savingsAccount = new SavingsAccount();
+//SavingsAccount savingsAccount = new SavingsAccount();
 
-savingsAccount.Deposit();
-savingsAccount.Withdraw();
-savingsAccount.Balance();
-savingsAccount.GetMessage();
+//savingsAccount.Deposit();
+//savingsAccount.Withdraw();
+//savingsAccount.Balance();
+//savingsAccount.GetMessage();
 
-Console.WriteLine("--------------------------------------------------");
+//Console.WriteLine("--------------------------------------------------");
 
-SavingAcc savingAcc = new SavingAcc();
-savingAcc.OpenAccount();
-savingAcc.Deposit();
-savingAcc.Withdraw();
-savingAcc.Balance();
-savingAcc.CloseAccount();
+//SavingAcc savingAcc = new SavingAcc();
+//savingAcc.OpenAccount();
+//savingAcc.Deposit();
+//savingAcc.Withdraw();
+//savingAcc.Balance();
+//savingAcc.CloseAccount();
+
+// A class é static, não pode ser instanciada
+//Calculate calculate = new Calculate();
+Console.WriteLine("Increment: " + Calculate.Increment());
+Console.WriteLine("Increment: " + Calculate.Increment());
+Console.WriteLine("Increment: " + Calculate.Increment());
+
+Console.WriteLine("Decrement: " + Calculate.Decrement());
 
 Console.ReadKey();
